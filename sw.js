fdf062d6040ee0d457baebdc65bd029479f37124
Gilberto Moreira <escolateologicavance@gmail.com>
2026-03-13 10:39:07 -0400

    Update sw.js

--- sw.js
+++ sw.js
@@ -1,17 +1,33 @@
-const CACHE_NAME = 'familia-firme-v2';
+const CACHE_NAME = 'familia-firme-v1';
 const assets = [
-  'index.html', '1.html', '2.html', '3.html', '4.html', '5.html', '6.html', '7.html', '8.html', 'manifest.json',
-  'capa-192.png', '1000301044.png', '1773162817019.jpeg', '1000301189.jpg', '1773174854728.jpeg'
+  'index.html', // Essencial para o PWA
+  '1.html',
+  '2.html',
+  '3.html',
+  '4.html',
+  '5.html',
+  '6.html',
+  '7.html',
+  '8.html',
+  '1000301044.png',
+  'capa-192.png', 
+  'capa-512.png', 
+  'manifest.json'
 ];
 
 self.addEventListener('install', event => {
   event.waitUntil(
-    caches.open(CACHE_NAME).then(cache => cache.addAll(assets))
+    caches.open(CACHE_NAME).then(cache => {
+      // Adicionamos um por um para garantir que não trave se uma imagem faltar
+      return cache.addAll(assets);
+    })
   );
 });
 
 self.addEventListener('fetch', event => {
   event.respondWith(
-    caches.match(event.request).then(response => response || fetch(event.request))
+    caches.match(event.request).then(response => {
+      return response || fetch(event.request);
+    })
   );
 });
